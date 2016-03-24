16110f655e73978faf7a7367bb29f800fdc7ea26
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-24 16:10:22 +0800

    fix autojump breaking cd

--- conf.d/__aj_add.fish
+++ conf.d/__aj_add.fish
@@ -1,5 +1,5 @@
 # change pwd hook
 function __aj_add --on-variable PWD
     status --is-command-substitution; and return
-    autojump --add $PWD >/dev/null 2>>$AUTOJUMP_ERROR_PATH &
+    autojump --add $PWD >/dev/null ^ $AUTOJUMP_ERROR_PATH &
 end
